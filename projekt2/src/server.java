c5608a17f370405abb26baa64a3a0377cb9f7af7
ronnaker <dat11jro@student.lu.se>
2014-02-16 16:20:45 +0100

    Hejhej

--- projekt2/src/server.java
+++ projekt2/src/server.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.security.KeyStore;
+
 import javax.net.*;
 import javax.net.ssl.*;
 import javax.security.cert.X509Certificate;
@@ -8,7 +9,8 @@ import javax.security.cert.X509Certificate;
 public class server implements Runnable {
     private ServerSocket serverSocket = null;
     private static int numConnectedClients = 0;
-
+    private static int Doctor = 0, Patient = 1, Nurse = 2, Government = 3;
+    
     public server(ServerSocket ss) throws IOException {
         serverSocket = ss;
         newListener();
@@ -17,15 +19,41 @@ public class server implements Runnable {
     public void run() {
         try {
             SSLSocket socket=(SSLSocket)serverSocket.accept();
-            newListener();
+            newListener();            
             SSLSession session = socket.getSession();
             X509Certificate cert = (X509Certificate)session.getPeerCertificateChain()[0];
             String subject = cert.getSubjectDN().getName();
     	    numConnectedClients++;
             System.out.println("client connected");
-            System.out.println("client name (cert subject DN field): " + subject);
+            System.out.println("client name (cert subject DN field): " + subject); //"CN=XXXXXX"
             System.out.println(numConnectedClients + " concurrent connection(s)\n");
-
+            String[] strings = subject.split("=");
+            String[] type = strings[1].split(" ");
+            
+            /*******************************LÄGG TILL ATT HÄMTA DIVISION!!!! JAAA*************************************************/
+            int currType = 0;
+            String name = "";
+            for(int i = 1; i < type.length; i++){
+            	name += type[i] + " ";
+            }
+            name = name.trim();
+            switch(type[0]){
+	            case "Dr" : 
+	            	currType = Doctor;
+	            	break;
+	            case "Nurse" : 
+	            	currType = Nurse;
+	            	break;
+	            case "Mr" : 
+	            	currType = Government;
+	            	break;
+	            case "Patient" : 
+	            	currType = Patient;
+	            	break;            
+            }
+            System.out.println("Type = " + currType + " name = " + name);
+            
+            
             PrintWriter out = null;
             BufferedReader in = null;
             out = new PrintWriter(socket.getOutputStream(), true);
@@ -33,14 +61,27 @@ public class server implements Runnable {
 
             String clientMsg = null;
             while ((clientMsg = in.readLine()) != null) {
+            	String[] message = clientMsg.split(" ");
+            	if(hasAccess(message[1], message[2], message[0], currType, name)){            		
+            		System.out.println("YES YOU CAN");
+            		out.println(1);            		
+            	}else{
+            		out.println(0);
+            	}
+            	out.flush();
+            	
+            	/*
 			    String rev = new StringBuilder(clientMsg).reverse().toString();
                 System.out.println("received '" + clientMsg + "' from client");
                 System.out.print("sending '" + rev + "' to client...");
 				out.println(rev);
-				out.flush();
+				out.flush();*/
                 System.out.println("done\n");
 			}
 			in.close();
+			
+				
+			
 			out.close();
 			socket.close();
     	    numConnectedClients--;
@@ -53,6 +94,42 @@ public class server implements Runnable {
         }
     }
 
+    private boolean hasAccess(String personnr, String date, String action, int currType, String name) {
+    	BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader("Patient Files/" + personnr + "/" + date));
+		} catch (FileNotFoundException e) {
+			return false;
+		}
+    	try {
+    	   StringBuilder sb = new StringBuilder();
+    	   String line = br.readLine();
+    	   if(line != null){
+    		  String[] accessPeople = line.split(",");    		  
+    		  for(String s : accessPeople){
+    			  String[] yay = s.split(" ");
+    			  
+    		  }
+    		   
+    	   }else{
+    		   return false;
+    	   }
+    	
+    	 }catch(Exception e){
+    	    	
+    	 }finally {
+    	    try {
+				br.close();
+			} catch (IOException e) {
+				
+			}
+    	  }
+    	
+    	
+    	
+    	return false;
+    }
+    
     private void newListener() { (new Thread(this)).start(); } // calls run()
 
     public static void main(String args[]) {
